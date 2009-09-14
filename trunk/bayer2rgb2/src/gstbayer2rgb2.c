b116d2ade73c8e3a7a7f3a3cb81ccb6f4a941578
anthony.violo <anthony.violo@a96dc9dc-a10c-11de-8af6-edd33a9b1062>
2009-09-14 09:50:03 +0000

    Added a plugin bayer2rgb2,jp462bayer and howto.txt

---
+++ trunk/bayer2rgb2/src/gstbayer2rgb2.c
@@ -0,0 +1,510 @@
+/*
+ * GStreamer
+ * Copyright (C) 2009 Anthony Violo <anthony.violo@ubicast.eu>
+ * Bayer pattern decoding functions from libdc1394 project
+ * ("http://damien.douxchamps.net/ieee1394/libdc1394/"), written by 
+ * Damien Douxchamps and Frederic Devernay; the original VNG and AHD Bayer 
+ * decoding are from Dave Coffin's DCRAW.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * SECTION:element-bayer2rgb2
+ *
+ * Converts raw bayer data to RGB images with selectable demosaicing methods and JP46 (Elphel) mode support.
+ *
+ * <refsect2>
+ * <title>Example launch line</title>
+ * |[
+ * gst-launch-0.10 filesrc location=test/fruits.jp4 ! jpegdec ! bayer2rgb2 jp46-mode=true method=6 ! ffmpegcolorspace ! freeze ! ximagesink
+ * ]|
+ * |[
+ * gst-launch-0.10 rtspsrc location=rtsp://192.168.1.9:554 protocols=0x00000001 ! rtpjpegdepay ! jpegdec ! queue ! jp462bayer 
+ * ! queue ! bayer2rgb2 ! queue ! ffmpegcolorspace ! videorate ! "video/x-raw-yuv, format=(fourcc)I420, width=(int)1920, 
+ * height=(int)1088, framerate=(fraction)25/1" ! xvimagesink sync=false max-lateness=-1 -v
+ * </refsect2>
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <string.h>
+#include "gstbayer2rgb2.h"
+
+GST_DEBUG_CATEGORY_STATIC (gst_bayer2rgb2_debug);
+#define GST_CAT_DEFAULT gst_bayer2rgb2_debug
+
+
+enum
+{
+  LAST_SIGNAL
+};
+
+enum
+  {
+    PROP_0,
+    PROP_METHODE,
+    PROP_FIRSTPIXEL,
+    PROP_BPP,
+    PROP_WIDTH,
+    PROP_HEIGHT,
+  };
+
+enum
+  {
+    NEAREST,
+    SIMPLE,
+    BILINEAR,
+    HQLINEAR,
+    DOWNSAMPLE,
+    EDGESENSE,
+    VNG,
+    AHD
+  };
+
+enum
+  {
+    RGGB,
+    GBRG,
+    GRBG,
+    BGGR
+  };
+
+static const GstElementDetails gst_alpha_color_details =
+GST_ELEMENT_DETAILS 
+  (
+   "Bayer to RGB conversion filter",
+   "Filter/Effect/Video",
+   "Converts raw bayer data to RGB images with selectable demosaicing methods and JP46 (Elphel) mode support",
+   "Anthony Violo <anthony.violo@ubicast.eu>"
+   );
+
+static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE 
+  ("sink",
+   GST_PAD_SINK,
+   GST_PAD_ALWAYS,
+   GST_STATIC_CAPS("ANY")
+   );
+
+static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE
+  ("src",
+   GST_PAD_SRC,
+   GST_PAD_ALWAYS,
+   GST_STATIC_CAPS
+   (GST_VIDEO_CAPS_RGB ";")
+   );
+
+
+#define DEBUG_INIT(bla) \
+  GST_DEBUG_CATEGORY_INIT (gst_bayer2rgb2_debug, "bayer2rgb", 0, "bayer2rgb element");
+
+GST_BOILERPLATE_FULL(GstBayer2rgb2, gst_bayer2rgb2, GstBaseTransform,
+		GST_TYPE_BASE_TRANSFORM, DEBUG_INIT);
+
+static void gst_bayer2rgb2_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec);
+static void gst_bayer2rgb2_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec);
+
+static GstFlowReturn gst_bayer2rgb2_transform
+(GstBaseTransform * pad, GstBuffer *inbuf, GstBuffer *outbuf);
+static GstCaps *gst_bayer2rgb2_transform_caps
+(GstBaseTransform * btrans, GstPadDirection direction, GstCaps * caps);
+static gboolean gst_bayer2rgb2_set_caps
+(GstBaseTransform * btrans, GstCaps * incaps, GstCaps * outcaps);
+static gboolean gst_bayer2rgb2_get_unit_size (GstBaseTransform * base,
+				     GstCaps * caps, guint * size);
+
+static GType
+gst_methode_pattern_get_type (int flag)
+{
+  static GType methode_pattern_type = 0;
+  static GType pixel_pattern_type = 0;
+
+  if (!flag)
+    {
+      if (!methode_pattern_type) 
+	{
+	  static GEnumValue pattern_types[] = 
+	    {
+	      { NEAREST, "Nearest", "Nearest" },
+	      { SIMPLE,  "Simple",  "Simple" },
+	      { BILINEAR, "Bilinear", "Bilinear" },
+	      { HQLINEAR, "Hqlinear", "Hqlinear" },
+	      { DOWNSAMPLE, "Downsample", "Downsample" },
+	      { EDGESENSE, "Edgesense", "Edgesense" }, 
+	      { VNG, "Vng", "Vng" },
+	      { AHD, "Ahd", "Ahd" },
+	      {0, NULL, NULL },
+	    };
+	  methode_pattern_type =
+	    g_enum_register_static ("GstMethodeRGB",
+				    pattern_types);
+	}
+      return methode_pattern_type;
+    }
+  if (!pixel_pattern_type) 
+    {
+      static GEnumValue pixel_types[] = 
+	{
+	  { RGGB, "RGGB", "RGGB" },
+	  { GBRG, "GBRG", "GBRG" },
+	  { GRBG, "GRGB", "GRGB" },
+	  { BGGR, "BGGR", "BGGR" },
+	  {0, NULL, NULL },
+	};
+      pixel_pattern_type =
+	g_enum_register_static ("GstPixelRGB",
+				pixel_types);
+    }
+  return pixel_pattern_type;
+}
+
+static void
+gst_bayer2rgb2_base_init (gpointer gclass)
+{
+  GstElementClass *element_class = GST_ELEMENT_CLASS (gclass);
+
+  gst_element_class_set_details(element_class, &gst_alpha_color_details);
+ 
+  gst_element_class_add_pad_template(element_class, 
+     gst_static_pad_template_get (&sink_factory));
+  gst_element_class_add_pad_template (element_class,
+     gst_static_pad_template_get (&src_factory));
+}
+
+static void
+gst_bayer2rgb2_class_init (GstBayer2rgb2Class * klass)
+{
+  GObjectClass		*gobject_class;
+  GstElementClass	*gstelement_class;
+  GstBaseTransformClass *gstbasetransform_class;
+
+  gobject_class = (GObjectClass *) klass;
+  
+  gstelement_class = (GstElementClass *) klass;
+  gstbasetransform_class = (GstBaseTransformClass *) klass;
+ 
+  gobject_class->set_property = gst_bayer2rgb2_set_property;
+  gobject_class->get_property = gst_bayer2rgb2_get_property;
+
+  gstbasetransform_class->transform_caps =
+    GST_DEBUG_FUNCPTR (gst_bayer2rgb2_transform_caps);
+
+  GST_BASE_TRANSFORM_CLASS (klass)->get_unit_size =
+      GST_DEBUG_FUNCPTR (gst_bayer2rgb2_get_unit_size);
+
+  gstbasetransform_class->set_caps =
+    GST_DEBUG_FUNCPTR (gst_bayer2rgb2_set_caps);
+
+  gstbasetransform_class->transform =
+    GST_DEBUG_FUNCPTR (gst_bayer2rgb2_transform);
+
+  g_object_class_install_property
+    (gobject_class, PROP_BPP,
+     g_param_spec_boolean 
+     ("bpp", "Bpp", "Bits per pixel (needed with raw sensor data): \n\t\t        FALSE = 8 bpp, TRUE = 16 bpp.",
+      FALSE, G_PARAM_READWRITE));
+  
+  g_object_class_install_property
+    (gobject_class, PROP_METHODE,
+     g_param_spec_enum 
+     ("method", "Method",
+      "Demosaicing interpolation algorithm",
+      gst_methode_pattern_get_type(0), 1, G_PARAM_READWRITE));
+
+   g_object_class_install_property
+    (gobject_class, PROP_FIRSTPIXEL,
+     g_param_spec_enum 
+     ("pixel-order", "Pixel-order",
+      "Pixel ordering template",
+      gst_methode_pattern_get_type(1), 2, G_PARAM_READWRITE));
+   
+   GST_DEBUG_CATEGORY_INIT (gst_bayer2rgb2_debug, "bayer2rgb2", 0,
+			    "YUV->RGB");
+}
+
+static void
+gst_bayer2rgb2_init (GstBayer2rgb2 * filter,
+		     GstBayer2rgb2Class * gclass)
+{
+  gst_base_transform_set_in_place (GST_BASE_TRANSFORM (filter), TRUE);
+  filter->header = 0;
+  filter->methode = 1;
+  filter->pixel = 2;
+  filter->width = 0;
+  filter->height = 0;
+  filter->bpp = FALSE;
+}
+
+static gboolean
+gst_bayer2rgb2_get_unit_size (GstBaseTransform * base, GstCaps * caps,
+    guint * size)
+{
+  GstStructure *structure;
+  int width;
+  int height;
+  int pixsize;
+  int i;
+  const char *name;
+
+  structure = gst_caps_get_structure (caps, 0);
+  if (gst_structure_get_int (structure, "width", &width) &&
+      gst_structure_get_int (structure, "height", &height)) 
+    {
+      name = gst_structure_get_name (structure);
+      if (strcmp (name, "video/x-raw-rgb")) 
+	{
+	  for (i = 0; size[i]; i++)
+	    *size = GST_ROUND_UP_4 (width) * height * 1.5;
+	  return TRUE;
+	}
+      else
+	{
+	  if (gst_structure_get_int (structure, "bpp", &pixsize)) 
+	    {
+	      *size = width * height * (pixsize / 8);
+	      return TRUE;
+	    }
+	}
+      
+    }
+  GST_ELEMENT_ERROR (base, CORE, NEGOTIATION, (NULL),
+		     ("Incomplete caps, some required field missing"));
+  return FALSE;
+}
+
+static GstCaps *gst_bayer2rgb2_transform_caps 
+(GstBaseTransform * btrans, GstPadDirection direction, GstCaps * caps)
+{
+  GstStructure *structure;
+  GstCaps *newcaps;
+  GstStructure *newstruct;
+
+  GST_DEBUG_OBJECT (caps, "transforming caps (from)");
+  structure = gst_caps_get_structure (caps, 0);
+  if (direction == GST_PAD_SRC)
+    newcaps = gst_caps_new_simple ("video/x-raw-yuv", NULL);
+  else
+    newcaps = gst_caps_new_simple ("video/x-raw-rgb", NULL);
+  newstruct = gst_caps_get_structure (newcaps, 0);
+  gst_structure_set_value (newstruct, "width",
+      gst_structure_get_value (structure, "width"));
+  gst_structure_set_value (newstruct, "height",
+      gst_structure_get_value (structure, "height"));
+  gst_structure_set_value (newstruct, "framerate",
+      gst_structure_get_value (structure, "framerate"));
+  GST_DEBUG_OBJECT (newcaps, "transforming caps (into)");
+  return newcaps;
+}
+
+static gboolean gst_bayer2rgb2_set_caps
+(GstBaseTransform * btrans, GstCaps * incaps, GstCaps * outcaps)
+{
+  GstBayer2rgb2 *alpha;
+  GstStructure *structure;
+  gboolean ret;
+  const GValue *fps;
+  gint w=0, h=0, depth, bpp, val;
+
+  alpha = GST_BAYER2RGB2 (btrans);
+  structure = gst_caps_get_structure (incaps, 0);
+  ret = gst_structure_get_int (structure, "width", &w);
+  alpha->width = w;
+  ret &= gst_structure_get_int (structure, "height", &h);
+  alpha->height = h;
+  fps = gst_structure_get_value (structure, "framerate");
+  ret &= (fps != NULL && GST_VALUE_HOLDS_FRACTION (fps));
+  structure = gst_caps_get_structure (outcaps, 0);
+  fps = gst_structure_get_value (structure, "framerate");
+  ret = (fps != NULL && GST_VALUE_HOLDS_FRACTION (fps));
+  ret &= gst_structure_get_int (structure, "bpp", &bpp);
+  ret &= gst_structure_get_int (structure, "red_mask", &val);
+  gst_structure_get_int (structure, "green_mask", &val);
+  gst_structure_get_int (structure, "blue_mask", &val);
+  ret &= gst_structure_get_int (structure, "depth", &depth);
+  if (!ret || val == 0 || depth != 24 || bpp != 24) 
+    {
+      GST_DEBUG_OBJECT (alpha, "incomplete or non-RGB input caps");
+      return FALSE;
+    }
+  return TRUE;
+}
+
+
+static void
+gst_bayer2rgb2_set_property (GObject * object, guint prop_id,
+			     const GValue * value, GParamSpec * pspec)
+{
+  GstBayer2rgb2 *filter = GST_BAYER2RGB2 (object);
+
+  switch (prop_id)
+    {
+    case PROP_METHODE:
+      filter->methode = g_value_get_enum (value);
+      break;
+    case PROP_FIRSTPIXEL:
+      filter->pixel = g_value_get_enum (value);
+      break;
+    case PROP_BPP:
+      filter->bpp = g_value_get_boolean (value);
+      break;
+    case PROP_WIDTH:
+      filter->width = g_value_get_int (value);
+      break;
+    case PROP_HEIGHT:
+      filter->height = g_value_get_int (value);
+    break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+static void
+gst_bayer2rgb2_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstBayer2rgb2 *filter = GST_BAYER2RGB2 (object);
+  switch (prop_id)
+    {
+    case PROP_METHODE:
+      g_value_set_enum (value, filter->methode);
+      break;
+    case PROP_FIRSTPIXEL:
+      g_value_set_enum (value, filter->pixel);
+      break;
+    case PROP_BPP:
+      g_value_set_boolean (value, filter->bpp);
+      break;
+    case PROP_WIDTH:
+      g_value_set_int (value, filter->width);
+    break;
+    case PROP_HEIGHT:
+      g_value_set_int (value, filter->height);
+    break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+uint8_t	*bayer2rgb2(GstBayer2rgb2 *filter, uint8_t *output)
+{
+  uint32_t	bpp = 0;
+  uint32_t	in_size = 0;
+  
+  bpp = filter->bpp ? 16 : 8;
+  in_size = filter->width * filter->height;
+  switch(bpp)
+    {
+    case 8:
+      dc1394_bayer_decoding_8bit(filter->tmp, output, filter->width, filter->height, filter->pixel, filter->methode);
+      break;
+    case 16:
+    default:
+      {
+	uint8_t		tmp = 0;
+	uint32_t	i = 0;
+	for(i = 0; i < in_size; i += 2)
+	  {
+	    tmp = *(((uint8_t*)filter->tmp) + i);
+	    *(((uint8_t*)filter->tmp) + i) = *(((uint8_t*)filter->tmp) + i + 1);
+	    *(((uint8_t*)filter->tmp) + i + 1) = tmp;
+	  }
+      }
+      dc1394_bayer_decoding_16bit((const uint16_t*)filter->tmp, (uint16_t*)output, filter->width, filter->height, filter->pixel, filter->methode, 16);
+      break;
+    }
+  return output;
+}
+  
+static GstFlowReturn
+gst_bayer2rgb2_transform(GstBaseTransform * pad, GstBuffer *inbuf, GstBuffer *outbuf)
+{
+  GstBayer2rgb2		*filter;
+  uint8_t		*input; 
+  uint8_t		*output;
+  int			i;
+
+  filter = GST_BAYER2RGB2(pad);
+  input = (uint8_t *) GST_BUFFER_DATA (inbuf);
+  output = (uint8_t *) GST_BUFFER_DATA (outbuf);
+  if (!(filter->tmp = malloc(1 + filter->width * filter->height * (sizeof(*filter->tmp)))))
+    return GST_FLOW_NOT_LINKED;
+  for (i = 0; i < filter->width * filter->height; i++)
+    filter->tmp[i] = input[i];
+  if (filter->pixel == 0)
+    filter->pixel = (guint16) DC1394_COLOR_FILTER_RGGB;
+  if (filter->pixel == 1)
+    filter->pixel = (guint16) DC1394_COLOR_FILTER_GBRG;
+  if (filter->pixel == 2)
+    filter->pixel = (guint16) DC1394_COLOR_FILTER_GRBG;
+  if (filter->pixel == 3)
+    filter->pixel = (guint16) DC1394_COLOR_FILTER_BGGR;
+  if (filter->height > 8 && filter->width > 8)
+    output = bayer2rgb2(filter, output);
+  g_free(filter->tmp);
+  return GST_FLOW_OK;
+}
+
+static gboolean
+bayer2rgb2_init (GstPlugin * bayer2rgb2)
+{
+  GST_DEBUG_CATEGORY_INIT (gst_bayer2rgb2_debug, "bayer2rgb2",
+       0, "bayer2rgb2");
+  return gst_element_register (bayer2rgb2, "bayer2rgb2", GST_RANK_NONE,
+      GST_TYPE_BAYER2RGB2);
+}
+
+GST_PLUGIN_DEFINE (
+    GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    "bayer2rgb2",
+    "Converts raw bayer data to rgb images",
+    bayer2rgb2_init,
+    VERSION,
+    "LGPL",
+    "GStreamer",
+    "http://www.ubicast.eu/"
+)
---
+++ trunk/jp462bayer/src/gstjp462bayer.c
@@ -0,0 +1,318 @@
+/*
+ * GStreamer
+ * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
+ * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
+ * Copyright (C) 2009 Anthony Violo <<user@hostname.org>>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * SECTION:element-jp462bayer
+ *
+ * FIXME:Describe jp462bayer here.
+ *
+ * <refsect2>
+ * <title>Example launch line</title>
+ * |[
+ * gst-launch -v -m fakesrc ! jp462bayer ! fakesink silent=TRUE
+ * ]|
+ * </refsect2>
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gst/gst.h>
+#include <string.h>
+#include <stdint.h>
+#include "gstjp462bayer.h"
+
+GST_DEBUG_CATEGORY_STATIC (gst_jp462bayer_debug);
+#define GST_CAT_DEFAULT gst_jp462bayer_debug
+
+/* Filter signals and args */
+enum
+{
+  /* FILL ME */
+  LAST_SIGNAL
+};
+
+enum
+{
+  PROP_0,
+  PROP_SILENT
+};
+
+/* the capabilities of the inputs and outputs.
+ *
+ * describe the real formats here.
+ */
+static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE ("sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("ANY")
+    );
+
+static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE ("src",
+    GST_PAD_SRC,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("ANY")
+    );
+
+GST_BOILERPLATE (GstJP462bayer, gst_jp462bayer, GstBaseTransform,
+    GST_TYPE_BASE_TRANSFORM);
+
+static void gst_jp462bayer_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec);
+static void gst_jp462bayer_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec);
+
+static gboolean gst_jp462bayer_set_caps (GstBaseTransform * btrans, GstCaps * incaps, GstCaps * outcaps);
+static GstFlowReturn gst_jp462bayer_transform(GstBaseTransform * pad, GstBuffer *inbuf, GstBuffer *outbuf);
+static gboolean gst_bayer2rgb2_get_unit_size (GstBaseTransform * base, GstCaps * caps, guint * size);
+
+/* GObject vmethod implementations */
+
+static void
+gst_jp462bayer_base_init (gpointer gclass)
+{
+  GstElementClass *element_class = GST_ELEMENT_CLASS (gclass);
+
+  gst_element_class_set_details_simple(element_class,
+    "JP462bayer",
+    "FIXME:Generic",
+    "FIXME:Generic Template Element",
+    "Anthony Violo <<user@hostname.org>>");
+
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&src_factory));
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&sink_factory));
+}
+
+/* initialize the jp462bayer's class */
+static void
+gst_jp462bayer_class_init (GstJP462bayerClass * klass)
+{
+  GObjectClass		*gobject_class;
+  GstElementClass	*gstelement_class;
+  GstBaseTransformClass *gstbasetransform_class;
+
+  gobject_class = (GObjectClass *) klass;
+  
+  gstelement_class = (GstElementClass *) klass;
+  gstbasetransform_class = (GstBaseTransformClass *) klass;
+ 
+  gobject_class->set_property = gst_jp462bayer_set_property;
+  gobject_class->get_property = gst_jp462bayer_get_property;
+
+  gstbasetransform_class->get_unit_size =
+    GST_DEBUG_FUNCPTR (gst_bayer2rgb2_get_unit_size);
+
+  gstbasetransform_class->set_caps =
+    GST_DEBUG_FUNCPTR (gst_jp462bayer_set_caps);
+
+  gstbasetransform_class->transform =
+    GST_DEBUG_FUNCPTR (gst_jp462bayer_transform);
+  
+  GST_DEBUG_CATEGORY_INIT (gst_jp462bayer_debug, "jp462bayer", 0,
+			    "YUV->Bayer");
+}
+
+/* initialize the new element
+ * instantiate pads and add them to element
+ * set pad calback functions
+ * initialize instance structure
+ */
+static void
+gst_jp462bayer_init (GstJP462bayer * filter,
+    GstJP462bayerClass * gclass)
+{
+  gst_base_transform_set_in_place (GST_BASE_TRANSFORM (filter), TRUE);
+}
+
+static void
+gst_jp462bayer_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstJP462bayer *filter = GST_JP462BAYER (object);
+
+  switch (prop_id) {
+    case PROP_SILENT:
+      filter->silent = g_value_get_boolean (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gst_jp462bayer_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstJP462bayer *filter = GST_JP462BAYER (object);
+
+  switch (prop_id) {
+    case PROP_SILENT:
+      g_value_set_boolean (value, filter->silent);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static gboolean
+gst_bayer2rgb2_get_unit_size (GstBaseTransform * base, GstCaps * caps,
+    guint * size)
+{
+  GstStructure *structure;
+  int width;
+  int height;
+  int pixsize;
+  int i;
+  const char *name;
+
+  structure = gst_caps_get_structure (caps, 0);
+  if (gst_structure_get_int (structure, "width", &width) &&
+      gst_structure_get_int (structure, "height", &height)) 
+    {
+      name = gst_structure_get_name (structure);
+      if (strcmp (name, "ANY")) 
+	{
+	  for (i = 0; size[i]; i++)
+	    *size = GST_ROUND_UP_4 (width) * height * 1.5;
+	  return TRUE;
+	}
+      else
+	{
+	  if (gst_structure_get_int (structure, "bpp", &pixsize)) 
+	    {
+	      *size = width * height;
+	      return TRUE;
+	    }
+	}
+      
+    }
+  GST_ELEMENT_ERROR (base, CORE, NEGOTIATION, (NULL),
+		     ("Incomplete caps, some required field missing"));
+  return FALSE;
+}
+
+static gboolean
+gst_jp462bayer_set_caps (GstBaseTransform *pad, GstCaps * incaps, GstCaps * outcaps)
+{
+  GstJP462bayer *filter;
+  GstStructure *structure;
+  gint w=0, h=0;
+
+  filter = GST_JP462BAYER (pad);
+  structure = gst_caps_get_structure (incaps, 0);
+  gst_structure_get_int (structure, "width", &w);
+  filter->width = w;
+  gst_structure_get_int (structure, "height", &h);
+  filter->height = h;
+  return TRUE;
+}
+
+static GstFlowReturn
+gst_jp462bayer_transform(GstBaseTransform * pad, GstBuffer *inbuf, GstBuffer *outbuf)
+{
+  GstJP462bayer		*filter;
+  guint32		abs;
+  guint32		ord;
+  int			i;
+  
+  filter = GST_JP462BAYER(pad);
+  if (filter->height < 8 || filter->width < 8 || filter->width % 16 != 0 || filter->height % 16 != 0)
+    {
+      GST_DEBUG_OBJECT(filter, "Size of image must be at least 8x8 and an multiple of 16", gst_flow_get_name(GST_FLOW_NOT_LINKED));
+      return GST_FLOW_NOT_LINKED;
+    }
+  for (i = 0; i < filter->width * filter->height; i++)
+    {
+      abs = ((i % (16 * filter->width / 16)) % 16) < 8 ? 
+	((((i % (16 * filter->width / 16)) % 16) * 2) + 16 * ((i % (16 * filter->width / 16)) / 16)) :
+	(((((i % (16 * filter->width / 16)) % 16) % 8 * 2) + 1 ) + 16 * ((i % (16 * filter->width / 16)) / 16));
+      ord = ((i / (16 * filter->width / 16)) % 16) < 8 ? 
+	((((i / (16 * filter->width / 16)) % 16) * 2) + 16 * (i / (256 * filter->width / 16))) :
+	(((((i / (16 * filter->width / 16)) % 16) % 8 * 2) + 1 ) + 16 * (i / (256 * filter->width / 16))); 
+      outbuf->data[(i / (256 * filter->width / 16)) * 256 * filter->width / 16 +
+		  (ord % 16) * filter->width + abs] = inbuf->data[i];
+    }
+  return  GST_FLOW_OK;
+}
+
+
+/* entry point to initialize the plug-in
+ * initialize the plug-in itself
+ * register the element factories and other features
+ */
+static gboolean
+jp462bayer_init (GstPlugin * jp462bayer)
+{
+  /* debug category for fltering log messages
+   *
+   * exchange the string 'Template jp462bayer' with your description
+   */
+  GST_DEBUG_CATEGORY_INIT (gst_jp462bayer_debug, "jp462bayer",
+      0, "Template jp462bayer");
+
+  return gst_element_register (jp462bayer, "jp462bayer", GST_RANK_NONE,
+      GST_TYPE_JP462BAYER);
+}
+
+/* gstreamer looks for this structure to register jp462bayers
+ *
+ * exchange the string 'Template jp462bayer' with your jp462bayer description
+ */
+GST_PLUGIN_DEFINE (
+    GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    "jp462bayer",
+    "Template jp462bayer",
+    jp462bayer_init,
+    VERSION,
+    "LGPL",
+    "GStreamer",
+    "http://gstreamer.net/"
+)
