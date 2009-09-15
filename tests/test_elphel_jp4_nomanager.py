a1404c03230791613ede7aa69855a4f0104c3883
fthiery <fthiery@a96dc9dc-a10c-11de-8af6-edd33a9b1062>
2009-09-15 15:19:29 +0000

    Reducing framerate to have real time processing possible

--- tests/test_elphel_jp4_nomanager.py
+++ tests/test_elphel_jp4_nomanager.py
@@ -1,7 +1,10 @@
 import os
-os.environ["GST_DEBUG"]="fake*:5"
+os.environ["GST_DEBUG"]="3"
 import gst
-t = "rtspsrc location=rtsp://192.168.1.9:554 ! rtpjpegdepay ! jpegdec ! queue ! jp462bayer ! queue ! bayer2rgb2 ! queue ! ffmpegcolorspace ! xvimagesink sync=false"
+
+output_caps = "video/x-raw-yuv, format=(fourcc)I420, width=(int)1920, height=(int)1088, framerate=(fraction)15/1"
+
+t = "rtspsrc location=rtsp://192.168.1.9:554 latency=10 ! rtpjpegdepay ! jpegdec ! videorate ! %s ! queue ! jp462bayer ! queue ! bayer2rgb2 ! queue ! ffmpegcolorspace ! xvimagesink sync=false" %(output_caps)
 p = gst.parse_launch(t)
 p.set_state(gst.STATE_PLAYING)
 import gtk
