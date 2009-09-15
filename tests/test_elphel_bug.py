965bea94c11a394bdfd295e008e811e6d2598d24
fthiery <fthiery@a96dc9dc-a10c-11de-8af6-edd33a9b1062>
2009-09-15 14:13:45 +0000

    Added performance tunings

--- tests/test_elphel_bug.py
+++ tests/test_elphel_bug.py
@@ -1,7 +1,7 @@
 import os
-os.environ["GST_DEBUG"]="3"
+os.environ["GST_DEBUG"]="fake*:5"
 import gst
-t = "rtspsrc location=rtsp://192.168.1.9:554 ! rtpjpegdepay ! jpegdec ! queue ! jp462bayer ! fakesink"
+t = "rtspsrc location=rtsp://192.168.1.9:554 ! rtpjpegdepay ! jpegdec ! queue ! jp462bayer ! queue ! bayer2rgb2 ! queue ! ffmpegcolorspace ! xvimagesink sync=false"
 p = gst.parse_launch(t)
 p.set_state(gst.STATE_PLAYING)
 import gtk
