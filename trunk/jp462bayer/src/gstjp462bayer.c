ebaf6514acf46632ffd36065439d2b4ec12626be
anthony.violo@gmail.com <anthony.violo@gmail.com@a96dc9dc-a10c-11de-8af6-edd33a9b1062>
2011-05-10 15:23:46 +0000

    add jp46 with image resising algo by 4 or 16

--- trunk/jp462bayer/src/gstjp462bayer.c
+++ trunk/jp462bayer/src/gstjp462bayer.c
@@ -63,12 +63,10 @@
 #endif
 
 #include <gst/gst.h>
+#include <gst/video/video.h>
 #include <string.h>
 #include <stdint.h>
 #include "gstjp462bayer.h"
-
-GST_DEBUG_CATEGORY_STATIC (gst_jp462bayer_debug);
-#define GST_CAT_DEFAULT gst_jp462bayer_debug
 
 /* Filter signals and args */
 enum
@@ -79,8 +77,8 @@ enum
 
 enum
 {
-  PROP_0,
-  PROP_SILENT
+	PROP_0,
+	PROP_SILENT,
 };
 
 /* the capabilities of the inputs and outputs.
@@ -109,7 +107,8 @@ static void gst_jp462bayer_get_property (GObject * object, guint prop_id,
 
 static gboolean gst_jp462bayer_set_caps (GstBaseTransform * btrans, GstCaps * incaps, GstCaps * outcaps);
 static GstFlowReturn gst_jp462bayer_transform(GstBaseTransform * pad, GstBuffer *inbuf, GstBuffer *outbuf);
-static gboolean gst_bayer2rgb2_get_unit_size (GstBaseTransform * base, GstCaps * caps, guint * size);
+static gboolean gst_jp462bayer_get_unit_size (GstBaseTransform * base, GstCaps * caps, guint * size);
+static GstCaps *gst_jp462bayer_transform_caps(GstBaseTransform * trans, GstPadDirection direction, GstCaps * caps);
 
 /* GObject vmethod implementations */
 
@@ -136,27 +135,93 @@ gst_jp462bayer_class_init (GstJP462bayerClass * klass)
 {
   GObjectClass		*gobject_class;
   GstElementClass	*gstelement_class;
-  GstBaseTransformClass *gstbasetransform_class;
+  GstBaseTransformClass *trans_class;
 
   gobject_class = (GObjectClass *) klass;
 
   gstelement_class = (GstElementClass *) klass;
-  gstbasetransform_class = (GstBaseTransformClass *) klass;
+  trans_class = (GstBaseTransformClass *) klass;
 
   gobject_class->set_property = gst_jp462bayer_set_property;
   gobject_class->get_property = gst_jp462bayer_get_property;
 
-  gstbasetransform_class->get_unit_size =
-    GST_DEBUG_FUNCPTR (gst_bayer2rgb2_get_unit_size);
+	trans_class->transform_caps = GST_DEBUG_FUNCPTR (gst_jp462bayer_transform_caps);
 
-  gstbasetransform_class->set_caps =
+  trans_class->get_unit_size =
+    GST_DEBUG_FUNCPTR (gst_jp462bayer_get_unit_size);
+
+  trans_class->set_caps =
     GST_DEBUG_FUNCPTR (gst_jp462bayer_set_caps);
 
-  gstbasetransform_class->transform =
+  trans_class->transform =
     GST_DEBUG_FUNCPTR (gst_jp462bayer_transform);
+}
 
-  GST_DEBUG_CATEGORY_INIT (gst_jp462bayer_debug, "jp462bayer", 0,
-			    "YUV->Bayer");
+static gboolean
+gst_jp462bayer_get_unit_size (GstBaseTransform * trans, GstCaps * caps, guint * size)
+{
+	GstStructure *structure;
+	int width;
+	int height;
+	int pixsize;
+	int i = 0;
+	const char *name;
+
+	structure = gst_caps_get_structure (caps, 0);
+	if (gst_structure_get_int (structure, "width", &width) &&
+      gst_structure_get_int (structure, "height", &height))
+    {
+		name = gst_structure_get_name (structure);
+		if (strcmp (name, "ANY"))
+		{
+	  		if (size[i])
+	    		for (i = 0; size[i]; i++)
+	      			*size = width * height * 1.5;
+	  		else
+	    		size[0] = GST_ROUND_UP_4 (width) * height * 1.5;
+	  	return TRUE;
+		}
+		else
+		{
+			if (gst_structure_get_int (structure, "bpp", &pixsize))
+	    	{
+	      	*size = width * height;
+	      	return TRUE;
+	    	}
+		}
+	}
+	//GST_ELEMENT_ERROR (base, CORE, NEGOTIATION, (NULL),
+		//     ("Incomplete caps, some required field missing"));
+	return FALSE;
+}
+
+static GstCaps *
+gst_jp462bayer_transform_caps (GstBaseTransform * trans,
+    GstPadDirection direction, GstCaps * caps)
+{
+  GstCaps *ret;
+  GstStructure *structure = NULL;
+
+  /* this function is always called with a simple caps */
+  g_return_val_if_fail (GST_CAPS_IS_SIMPLE (caps), NULL);
+
+  GST_DEBUG_OBJECT (trans,
+      "Transforming caps %" GST_PTR_FORMAT " in direction %s", caps,
+      (direction == GST_PAD_SINK) ? "sink" : "src");
+
+  ret = gst_caps_copy (caps);
+		structure = gst_structure_copy (gst_caps_get_structure (ret, 0));
+	gst_structure_set (structure,
+      		"width", GST_TYPE_INT_RANGE, 1, G_MAXINT,
+      		"height", GST_TYPE_INT_RANGE, 1, G_MAXINT, NULL);
+		gst_caps_append_structure (ret, structure);
+  /* if pixel aspect ratio, make a range of it */
+  if (gst_structure_has_field (structure, "pixel-aspect-ratio")) {
+	gst_structure_set (structure, "pixel-aspect-ratio", GST_TYPE_FRACTION_RANGE,
+	1, G_MAXINT, G_MAXINT, 1, NULL);
+  }
+  GST_DEBUG_OBJECT (trans, "returning caps: %" GST_PTR_FORMAT, ret);
+  return ret;
 }
 
 /* initialize the new element
@@ -204,99 +269,87 @@ gst_jp462bayer_get_property (GObject * object, guint prop_id,
 }
 
 static gboolean
-gst_bayer2rgb2_get_unit_size (GstBaseTransform * base, GstCaps * caps,
-    guint * size)
-{
-  GstStructure *structure;
-  int width;
-  int height;
-  int pixsize;
-  int i = 0;
-  const char *name;
-
-  structure = gst_caps_get_structure (caps, 0);
-  if (gst_structure_get_int (structure, "width", &width) &&
-      gst_structure_get_int (structure, "height", &height))
-    {
-      name = gst_structure_get_name (structure);
-      if (strcmp (name, "ANY"))
-	{
-	  if (size[i])
-	    for (i = 0; size[i]; i++)
-	      *size = width * height * 1.5;
-	  else
-	    size[0] = GST_ROUND_UP_4 (width) * height * 1.5;
-	  return TRUE;
-	}
-      else
-	{
-	  if (gst_structure_get_int (structure, "bpp", &pixsize))
-	    {
-	      *size = width * height * 1.5;
-	      return TRUE;
-	    }
-	}
-    }
-  GST_ELEMENT_ERROR (base, CORE, NEGOTIATION, (NULL),
-		     ("Incomplete caps, some required field missing"));
-  return FALSE;
-}
-
-static gboolean
 gst_jp462bayer_set_caps (GstBaseTransform *pad, GstCaps * incaps, GstCaps * outcaps)
 {
-  GstJP462bayer *filter;
-  GstStructure *structure;
-  gint w=0, h=0;
+	GstJP462bayer 	*filter;
+	GstStructure 	*structure;
+	gint 			width_in=0, height_in=0;
+	gint 			width_out=0, height_out=0;
+	float 			size;
+	int				i, j;
 
-  filter = GST_JP462BAYER (pad);
-  structure = gst_caps_get_structure (incaps, 0);
-  gst_structure_get_int (structure, "width", &w);
-  filter->width = w;
-  gst_structure_get_int (structure, "height", &h);
-  filter->height = h;
-  return TRUE;
+	filter = GST_JP462BAYER (pad);
+	structure = gst_caps_get_structure (incaps, 0);
+	gst_structure_get_int (structure, "width", &width_in);
+	gst_structure_get_int (structure, "height", &height_in);
+	filter->width = width_in;
+	filter->height = height_in;
+	structure = gst_caps_get_structure (outcaps, 0);
+	gst_structure_get_int (structure, "width", &width_out);
+	gst_structure_get_int (structure, "height", &height_out);
+	if 	(width_in == width_out && height_in == height_out)
+		filter->size = 1;
+	else if ((float)width_in / (float)width_out == 2.0 && (float)height_in / (float)height_out  == 2.0)
+		filter->size = 2;
+	else if ((float)width_in / (float)width_out == 4.0 && (float)height_in / (float)height_out  == 4.0)
+		filter->size = 4;
+	else
+	{
+		GST_ERROR("you should put output resolution divisible by 1, 2, 3 or 4");
+		return FALSE;
+	}
+	size = (filter->size == 1) ? 0.5 : (filter->size == 2) ? 1.0 : 2.0;
+	for (i = 0, j = 0; i < 16; i++, j++)
+    {
+		filter->index1[i] = (i % 2 == 1) ? (filter->index1[i - 1] + 8) : (i * size);
+		filter->index2[j] = filter->index1[j] * filter->width;
+    }
+    return TRUE;
+}
+
+int				get_value(guint32 x, guint32 value1, guint32 value2, guint32 b_of, GstJP462bayer *f, guint8 *data)
+{
+	guint32		value = 0;
+	guint8	k, l, m;
+
+	for (l = 0, m = 0; l < f->size; l++)
+		for (k = 0; k < f->size; k++, m++)
+				value += data[x + value1 + value2 + k + b_of + f->width * l];
+	return value / (f->size << (f->size / 2));
 }
 
 static GstFlowReturn
 gst_jp462bayer_transform(GstBaseTransform * pad, GstBuffer *inbuf, GstBuffer *outbuf)
 {
-  GstJP462bayer		*filter;
-  guint32		y, x;
-  guint32		b_of = 0;
-  guint32		h_of;
-  uint32_t		i, j;
-  guint32		index1[16]={0,8,1,9,2,10,3,11,4,12,5,13,6,14,7,15};
-  guint32		index2[16];
+	GstJP462bayer		*filter;
+	guint8 				i, j;
+	guint32				value;
+	guint32				y, x;
+	guint32				b_of;
+	guint32				h_of;
 
-  filter = GST_JP462BAYER(pad);
-  for (j = 0;j < 16; ++j)
-    index2[j] = index1[j] * filter->width;
-  for (y = 0; y < filter->height; y += 16, b_of += filter->width << 4)
-    for (x = 0; x < filter->width; x += 16)
-      for (j = 0, h_of = 0; j < 16; ++j, h_of += filter->width)
-	for (i = 0; i < 16; ++i)
-	  outbuf->data[x + i + h_of + b_of] = inbuf->data[x + index1[i] + index2[j] + b_of];
-  return  GST_FLOW_OK;
+	filter = GST_JP462BAYER(pad);
+	for (y = 0, b_of = 0, value = 0; y < filter->height; y += 16, b_of += filter->width << 4)
+			for (x = 0; x < filter->width; x += 16)
+				for (j = 0, h_of = 0; j < (16 / filter->size); ++j, h_of += filter->width)
+					for (i = 0; i < (16 / filter->size); ++i)
+					{
+						value  = (filter->size == 1) ? (inbuf->data[x + filter->index1[i] + filter->index2[j] + b_of]) :
+									get_value(x, filter->index1[i], filter->index2[j], b_of, filter, inbuf->data);
+						outbuf->data[(x / filter->size) + i + (h_of  / filter->size) + (b_of / (filter->size << filter->size / 2))] = value;
+					}
+	return  GST_FLOW_OK;
 }
 
-
-/* entry point to initialize the plug-in
- * initialize the plug-in itself
- * register the element factories and other features
- */
 static gboolean
 jp462bayer_init (GstPlugin * jp462bayer)
 {
-  /* debug category for fltering log messages
-   *
-   * exchange the string 'Template jp462bayer' with your description
-   */
-  GST_DEBUG_CATEGORY_INIT (gst_jp462bayer_debug, "jp462bayer",
-      0, "Template jp462bayer");
-
-  return gst_element_register (jp462bayer, "jp462bayer", GST_RANK_NONE,
-      GST_TYPE_JP462BAYER);
+  if (!gst_element_register (jp462bayer, "jp462bayer", GST_RANK_NONE,
+          GST_TYPE_JP462BAYER))
+    return FALSE;
+  GST_DEBUG_CATEGORY_INIT (gst_jp462bayer_debug, "jp462bayer", 0,
+      "jp462bayer element");
+  return TRUE;
 }
 
 /* gstreamer looks for this structure to register jp462bayers
@@ -314,3 +367,4 @@ GST_PLUGIN_DEFINE (
     "GStreamer",
     "http://www.ubicast.eu/"
 )
+
